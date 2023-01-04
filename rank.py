9cee02597e127ca778b421fb8a0d3265dbad6757
night <none@none.com>
2023-01-04 07:31:06 +0100

    Ajout help (souci car help déjà présente)

--- rank.py
+++ rank.py
@@ -7,7 +7,31 @@ import re
 # User Agent lambda pour pas avoir de 429
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-def get_user_rank(username):
+def get_user_help(command=None):
+	base_message = """
+	Un bot discord pour accompagner votre progression sur la plateforme www.root-me.org
+
+	rank            <username>
+	profile         <username>
+
+	Tapez !help commande pour plus d'information sur <commande>	
+	"""
+
+	if command is None:
+		return base_message
+	
+	elif command == "rank":
+		message = "La commande rank vous permet de voir le classement d'un utilisateur sur la plateforme www.root-me.org"
+		return message
+
+	elif command == "profile":
+		message = "La commande profile vous permet de voir le profil d'un utilisateur sur la plateforme www.root-me.org"
+		return message
+	else:
+		message = "Commande non reconnue. Voici la liste des commandes disponibles:"
+		return base_message
+
+def get_user_rank(username=None):
 	response = requests.get(f"https://api.www.root-me.org/user/{username}", headers=headers)
 	if response.status_code == 200:
 		pattern_points = r"<h3><img src='squelettes/img/valid.svg\?\d+' width='\d+' height='\d+' />&nbsp;(\d+)</h3>"
@@ -22,7 +46,7 @@ def get_user_rank(username):
 		time.sleep(0.2)  # Délai entre chaque requête
 		return "Error retrieving user ranking"
 
-def get_user_profile(username):
+def get_user_profile(username=None):
 	scores = []
 	categories = ["Web-Client","Programmation","Cryptanalyse","Steganographie","Web-Serveur","Cracking","Realiste","Reseau","App-Script","App-Systeme","Forensic"]
 	response = requests.get(f"https://api.www.root-me.org/user/{username}", headers=headers)
